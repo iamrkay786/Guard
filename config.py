5f07a74b23e313a7e9efa90ba89cced9f6b1ef62
ifeelraam <aditya20278@gmail.com>
2025-02-12 18:05:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 SPOILER_MODE = os.environ.get("SPOILER_MODE", "True").lower() == "true"
+MONGO_URI = os.environ.get("MONGO_URI", "")  
+SUDO_USERS = [6257927828]
